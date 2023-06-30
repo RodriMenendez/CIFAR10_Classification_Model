122295e084c524c938994592aa7f50ff815a25c2
Angel Avelar <rodri@Angels-MacBook-Air.local>
2023-06-30 12:35:13 -0700

    added wandb logger

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 import lightning as L
+from lightning.pytorch.loggers import WandbLogger
 
 import get_data
 import models
@@ -17,11 +18,19 @@ train_data, trainloader, test_data, testloader = get_data.get_CIFAR10()
 resnet_model = models.ResNet18()
 clf_model = models.Classification(resnet_model)
 
+# wandb
+wandb_config = {
+        "dataset": "CIFAR10",
+        "architecture": "Resnet18+HiddenLayer"
+    }
+
+wandb_logger = WandbLogger(project='CIFAR10-Resnet18-Classification', config=wandb_config)
+
 # lightning module
 autoencoder = lightning_modules.CIFARModule(clf_model)
 
 # train
-trainer = L.pytorch.Trainer(accelerator='auto', max_epochs=4)
+trainer = L.pytorch.Trainer(accelerator='auto', max_epochs=4, trainer=wandb_logger)
 trainer.fit(model=autoencoder, train_dataloaders=trainloader)
 
 # test
